63e62c24ff0da2ba395c460dfc235fb24378e6fc
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-07-01 10:57:47 +0800

    Update DBConnect.java

--- P2P/DBConnect.java
+++ P2P/DBConnect.java
@@ -2,14 +2,14 @@ package P2P;
 
 public class DBConnect {
     static String driver = null;
-    static String host = null; // 選擇需要操作的資料庫
+    static String host = null;
     static String uName = null;
     static String uPass = null;
 
     public DBConnect() {
         this.driver = "com.mysql.cj.jdbc.Driver";
-        this.host = "jdbc:mysql://localhost:3306/p2p";
-        this.uName = "root";
-        this.uPass = "rootroot";
+        this.host = "jdbc:mysql://localhost:3306/p2p"; // 選擇需要操作的資料庫
+        this.uName = ""; // 管理者帳號
+        this.uPass = ""; // 管理者密碼
     }
 }
