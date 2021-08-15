f1a0a43bd756ac44230e3f828f1d869db4fed876
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-08-15 13:27:17 +0800

    Create FoodOutputBo.java

---
+++ ConsoleMVC_CRUD/com/example/bean/FoodOutputBo.java
@@ -0,0 +1,31 @@
+package com.example.bean;
+
+public class FoodOutputBo {
+    private int foodPrice;
+    private String foodName;
+    private String foodType;
+
+    public void setPrice(int foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public int getPrice() {
+        return foodPrice;
+    }
+
+    public void setName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getName() {
+        return foodName;
+    }
+
+    public void setType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public String getType() {
+        return foodType;
+    }
+}
