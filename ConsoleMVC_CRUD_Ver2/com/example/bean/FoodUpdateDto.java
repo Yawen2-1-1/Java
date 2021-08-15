d83ee6b5908cece735c4facfeb052dd925958564
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-08-15 22:14:51 +0800

    Create FoodUpdateDto.java

---
+++ ConsoleMVC_CRUD_Ver2/com/example/bean/FoodUpdateDto.java
@@ -0,0 +1,32 @@
+package com.example.bean;
+
+public class FoodUpdateDto {
+
+    private String foodType;
+    private String foodProperty;
+    private String foodValue;
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodProperty(String foodProperty) {
+        this.foodProperty = foodProperty;
+    }
+
+    public String getFoodProperty() {
+        return foodProperty;
+    }
+
+    public void setFoodValue(String foodValue) {
+        this.foodValue = foodValue;
+    }
+
+    public String getFoodValue() {
+        return foodValue;
+    }
+}
