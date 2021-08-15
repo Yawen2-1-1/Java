9d82ca427ae15555ee40fb78b05807e3e21cf960
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-08-15 22:15:34 +0800

    Create SeaFoodDo.java

---
+++ ConsoleMVC_CRUD_Ver2/com/example/bean/SeaFoodDo.java
@@ -0,0 +1,23 @@
+package com.example.bean;
+
+public class SeaFoodDo {
+
+    private String foodName;
+    private String foodPrice;
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodPrice(String foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public String getFoodPrice() {
+        return foodPrice;
+    }
+}
