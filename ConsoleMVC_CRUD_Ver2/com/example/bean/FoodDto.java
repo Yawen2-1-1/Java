970dcfaa00742c1faf904de923d19e698b3c3094
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-08-15 22:12:28 +0800

    Create FoodDto.java

---
+++ ConsoleMVC_CRUD_Ver2/com/example/bean/FoodDto.java
@@ -0,0 +1,14 @@
+package com.example.bean;
+
+public class FoodDto {
+
+    private String foodType;
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+}
