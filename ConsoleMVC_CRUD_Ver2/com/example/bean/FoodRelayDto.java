862f9750ac45d98c1aac2b77c7401ed02b6e1b91
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-08-15 22:13:48 +0800

    Create FoodRelayDto.java

---
+++ ConsoleMVC_CRUD_Ver2/com/example/bean/FoodRelayDto.java
@@ -0,0 +1,32 @@
+package com.example.bean;
+
+public class FoodRelayDto {
+
+    private String foodType;
+    private String foodName;
+    private int foodPrice;
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodPrice(int foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public int getFoodPrice() {
+        return foodPrice;
+    }
+}
