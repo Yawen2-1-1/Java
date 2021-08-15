bc7b73d2e2ca761aeb889ad4cc34cb582dbce401
Yawen2-1-1 <62700679+Yawen2-1-1@users.noreply.github.com>
2021-08-15 13:26:40 +0800

    Create FoodInputBo.java

---
+++ ConsoleMVC_CRUD/com/example/bean/FoodInputBo.java
@@ -0,0 +1,14 @@
+package com.example.bean;
+
+public class FoodInputBo {
+
+    private String foodType;
+
+    public void setType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public String getType() {
+        return foodType;
+    }
+}
